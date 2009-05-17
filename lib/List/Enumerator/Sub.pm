5878057336e149c78b693f4a93c6ffe5e5068cdc
cho45 (さとう) <cho45@lowreal.net>
2009-05-17 19:41:12 +0000

    Import of SATOH/List-Enumerator-0.07 from CPAN.
    gitpan-cpan-distribution: List-Enumerator
    gitpan-cpan-version:      0.07
    gitpan-cpan-path:         SATOH/List-Enumerator-0.07.tar.gz
    gitpan-cpan-author:       SATOH
    gitpan-cpan-maturity:     released

---
+++ lib/List/Enumerator/Sub.pm
@@ -0,0 +1,54 @@
+package List::Enumerator::Sub;
+use base qw/List::Enumerator::Role/;
+use overload
+	'@{}' => \&getarray;
+
+__PACKAGE__->mk_accessors(qw/next_sub rewind_sub/);
+
+sub BUILD {
+	my ($self, $params) = @_;
+	
+	$self->next_sub($params->{next});
+	$self->rewind_sub($params->{rewind} || sub {});
+}
+
+sub _next {
+	my ($self, $new) = @_;
+
+	local $_ = $self;
+	$self->next_sub->($self);
+}
+
+sub _rewind {
+	my ($self, $new) = @_;
+
+	local $_ = $self;
+	$self->rewind_sub->($self);
+	$self;
+}
+
+sub getarray {
+	my ($self) = @_;
+	my @temp;
+	tie @temp, __PACKAGE__, $self;
+	\@temp;
+}
+
+sub TIEARRAY {
+	my ($class, $arg) = @_;
+	bless $arg, $class;
+}
+
+sub FETCHSIZE {
+	0;
+}
+
+sub FETCH { #TODO orz orz orz
+	my ($self, $index) = @_;
+	$self->rewind;
+	$self->next while ($index--);
+	$self->next;
+}
+
+1;
+__END__
